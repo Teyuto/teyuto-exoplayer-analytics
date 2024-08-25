36980d431ca02cd154b8d728cf5e7a76b009da8e
koomingchin <kmc@gms.com.my>
2024-08-25 20:08:55 +0800

    Fix media type error

--- app/src/main/java/com/teyuto/exoplayeranalytics/TeyutoPlayerAnalytics.kt
+++ app/src/main/java/com/teyuto/exoplayeranalytics/TeyutoPlayerAnalytics.kt
@@ -2,7 +2,14 @@ package com.teyuto.exoplayeranalytics
 
 import android.os.Handler
 import android.os.Looper
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
